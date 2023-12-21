1b7c38491226711bab3087a6eb48cf40494c16b9
noppakornn <noppakorn.5142@gmail.com>
2023-12-21 14:18:10 +0700

    update

--- furniture.js
+++ furniture.js
@@ -2,8 +2,8 @@ let furniture = ['Table', 'Chairs','Couch'];
 
 for (let char of furniture) {
     for (let index = 0; index < char.length; index++) {
-        const element = char[index];
-        console.log(element);
+        const object = char[index];
+        console.log(object);
     }
     console.log("");
 }
