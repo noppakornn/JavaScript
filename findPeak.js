656fd756ee37cf1ffd0baa58d8016abf487ab8b3
noppakornn <noppakorn.5142@gmail.com>
2024-01-08 09:49:40 +0700

    ...

---
+++ findPeak.js
@@ -0,0 +1,20 @@
+function findAllPeakElements(nums) {
+    const result = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if ((i === 0 || nums[i] > nums[i - 1]) && (i === nums.length - 1 || nums[i] > nums[i + 1])) {
+            result.push(i);
+        }
+    }
+
+    return result;
+}
+
+// Example usage
+const nums1 = [3, 2, 3, 1];
+const nums2 = [1, 2, 1, 3, 5, 6, 4];
+const nums3 = [1, 2, 1, 4, 5, 6, 4, 7];
+
+console.log(findPeakIndices(nums1)); // Output: [0, 2]
+console.log(findPeakIndices(nums2)); // Output: [1, 5]
+console.log(findPeakIndices(nums3)); // Output: [1, 5, 7]
